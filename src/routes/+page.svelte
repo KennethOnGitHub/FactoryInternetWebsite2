3679cc9ca0e7b4a6a5953472e52a2e9f31fda1c8
Kenny <khnsmnplwn@gmail.com>
2024-04-08 01:12:56 +0100

    started work on header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Header from './Header.svelte';
+    import './reset.css'
+    import './styles.css'
+</script>
+
+<Header />  
+
+<h1 id="1">Cybersecurity for when certainty is a must</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1 id="2">Threats</h1>
+<h1 id="3">What we do</h1>
+
---
+++ src/routes/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+    import logo from '$lib/assets/Factory-Internet-Logo-2019-220x47.webp';
+
+</script>
+
+<header>
+    <a href="https://www.factoryinternet.co.uk/"> <img src =  {logo} alt = 'Factory Internet'> </a>
+    <nav>
+        <a href="#1">Threats</a>
+        <a href="#2">Home</a>
+        <a href="#3">What We Do</a>
+    </nav>
+</header>
+
+<style>
+    header {
+        display: flex;
+        background-color: #086ad8;
+        padding: 10px 0px 10px 20px;
+    }
+
+    nav {
+        display: flex;
+        flex-grow: 1;
+        justify-content: space-evenly;
+        color: white;
+    }
+</style>
